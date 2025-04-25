01b6d75d4fcb8e0d3eb80a1470d6e445fd57050d
Lahiru Jayawardhane <93923625+lahirujay99@users.noreply.github.com>
2025-04-25 23:21:20 +0530

    add migration for flower guess event

---
+++ prisma/migrations/20250425174903_add_flower_response_table/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "FlowerResponse" (
+    "id" TEXT NOT NULL,
+    "contest_type" TEXT NOT NULL,
+    "full_name" TEXT NOT NULL,
+    "nic" TEXT,
+    "contact_number" TEXT NOT NULL,
+    "secret_flower_name" TEXT NOT NULL,
+    "submitted_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "token_id" TEXT NOT NULL,
+
+    CONSTRAINT "FlowerResponse_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FlowerResponse_token_id_key" ON "FlowerResponse"("token_id");
+
+-- CreateIndex
+CREATE INDEX "FlowerResponse_token_id_idx" ON "FlowerResponse"("token_id");
+
+-- CreateIndex
+CREATE INDEX "FlowerResponse_contest_type_idx" ON "FlowerResponse"("contest_type");
+
+-- AddForeignKey
+ALTER TABLE "FlowerResponse" ADD CONSTRAINT "FlowerResponse_token_id_fkey" FOREIGN KEY ("token_id") REFERENCES "Token"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
